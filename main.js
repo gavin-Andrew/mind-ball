70c0374d22bddfcb5f4796b87a34c624d65ff24c
Gavin Andrew <xug84684@gmail.com>
2026-03-19 19:56:27 +0800

    add mind ball prototype UI

--- main.js
+++ main.js
@@ -1,35 +1,23 @@
-const { app, BrowserWindow } = require('electron');
-
-let mainWindow;
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 function createWindow() {
-  console.log('开始创建窗口');
+  const win = new BrowserWindow({
+    width: 220,
+    height: 220,
+    transparent: true,
+    frame: false,
+    resizable: false,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
 
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    show: true,
-    backgroundColor: '#ffffff'
-  });
-
-  console.log('窗口创建完成');
-
-  mainWindow.loadURL('data:text/html;charset=UTF-8,<h1>Hello Mind Ball</h1>');
-
-  mainWindow.on('closed', () => {
-    console.log('窗口已关闭');
-    mainWindow = null;
-  });
+  win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-  console.log('Electron 已准备好');
-  createWindow();
-});
-
-app.on('window-all-closed', () => {
-  console.log('所有窗口都关闭了');
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+  createWindow()
+})
